d80ff42f6f3bfdab5a0601d0748b35fd566f44b0
Seokkyung.lee <ec2-user@ip-172-31-7-196.us-east-2.compute.internal>
2018-06-04 05:45:42 +0000

    CRUD message

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta chrest="UTF-8">
+        <title>Tasklist</title>
+        </head>
+        <body>
+            @yield('content')
+            </body>
+    </html>
